a14a02e33f7784cc6e658175a8d4cf0cda0dbfa4
Bolaji Ayodeji <sirbeejay1@gmail.com>
2018-12-03 17:53:42 +0100

    made fixes

--- public/js/app.js
+++ public/js/app.js
@@ -1,24 +1,26 @@
-(function() {
+(function () {
   'use strict';
 
-  window.onload = function() {
-    let content = localStorage.getItem("qrCodeMessage") || 'QR Codes are great!';
+  window.onload = function () {
+    let content = localStorage.getItem("qrCodeMessage") || '';
     $('#qrCodeMessage').val(content);
     $('#qrcode').qrcode(content);
   }
 
-   $('#buttonGenerate').click(() => {
-     console.log('click');
-     let message = $('#qrCodeMessage').val();
-     localStorage.setItem("qrCodeMessage", message);
-     $('#qrcode').html('');
-     $('#qrcode').qrcode(message);
-   });
+  $('#buttonGenerate').click(() => {
+    console.log('click');
+    let message = $('#qrCodeMessage').val();
+    localStorage.setItem("qrCodeMessage", message);
+    $('#qrcode').html('');
+    $('#qrcode').qrcode(message);
+  });
 
 
   if ('serviceWorker' in navigator) {
     navigator.serviceWorker
-             .register('./service-worker.js')
-             .then(function() { console.log('Service Worker Registered'); });
+      .register('./service-worker.js')
+      .then(function () {
+        console.log('Service Worker Registered');
+      });
   }
 })();
